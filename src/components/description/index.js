c3b7006fa24bd1d520927f6dbb84a0f5290576dc
CAIO CESAR SANCHES <caio.csanches@senacsp.edu.br>
2022-08-11 20:25:08 -0300

    responsividade

--- src/components/description/index.js
+++ src/components/description/index.js
@@ -5,20 +5,11 @@ function Description() {
     return(
         <div class="description-area">
                 <div class="description-titulo">
-<<<<<<< HEAD
                    
                 </div>
                 <div class="description-content">
                     projeto tap game senac 40 anos
                     bla bla bla bla bla bla bla bla
-=======
-                    um mundo de aventuras
-                </div>
-                <div class="description-content">
-                    convide seus amigos pra um momento de
-                    diversão nesse mundo de aventuras
-                    cançando os monstros
->>>>>>> ec6cd92be863b1b69b38e0230955b4bf2567dd26
                     <div class="description-btn">
                         <a href="http://itch.io/" target="_blank">DOWNLOAD</a>
                     </div>
